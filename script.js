8f7f1449ca8ca3151e333482891c744e2bd3ef1a
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-20 14:52:44 +0530

    added header and param conatainer actions

--- script.js
+++ script.js
@@ -1,2 +1,34 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+const keyValueTemplate = document.querySelector("[data-key-value-template]");
+const queryParamsContainer = document.querySelector("[data-query-params]");
+const requestHeadersContainer = document.querySelector(
+  "[data-request-headers]"
+);
+
+const createKeyValuePair = () => {
+  const elem = keyValueTemplate.content.cloneNode(true);
+  elem
+    .querySelector("[data-remove-btn]")
+    .addEventListener("click", (event) =>
+      event.target.closest("[data-key-value-pair]").remove()
+    );
+
+  return elem;
+};
+
+queryParamsContainer.appendChild(createKeyValuePair());
+requestHeadersContainer.appendChild(createKeyValuePair());
+
+document
+  .querySelector("[data-add-query-param-btn]")
+  .addEventListener("click", () =>
+    queryParamsContainer.appendChild(createKeyValuePair())
+  );
+
+document
+  .querySelector("[data-add-request-header-btn]")
+  .addEventListener("click", () =>
+    requestHeadersContainer.appendChild(createKeyValuePair())
+  );
