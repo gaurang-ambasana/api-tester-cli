774353d8eeb4cfb14389001d606b63d309da74c8
Gaurang Ambasana <gpambasana@gmail.com>
2022-03-21 17:47:36 +0530

    deleted testing file

--- server.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-
-const app = express();
-
-app.use(express.static("../api-tester-cli/"));
-app.get("/", (req, res) => res.sendFile("./index.html"));
-
-app.listen(3000, () => console.log("started"));
